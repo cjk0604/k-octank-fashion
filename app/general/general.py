cea7b223b039d77313aeabcd13276a440e87a31b
Kim <kimcj@147dda415aea.ant.amazon.com>
2021-04-12 02:13:17 +0900

    for load test

--- app/general/general.py
+++ app/general/general.py
@@ -2,6 +2,30 @@ from flask import Flask, Blueprint, render_template, request, jsonify, url_for, 
 
 import requests
 import json
+
+import pymysql
+import os
+
+rds_host = os.environ['DATABASE_HOST']
+db_user = os.environ['DATABASE_USER']
+password = os.environ['DATABASE_PASSWORD']
+db_name = os.environ['DATABASE_DB_NAME']
+port = 3306
+
+conn = pymysql.connect(rds_host, user=db_user, passwd=password, db=db_name, connect_timeout=5, port=port, charset='utf8mb4')
+
+try:
+    with conn.cursor(pymysql.cursors.DictCursor) as cursor:
+        sql = 'select * from fashion'
+        cursor.execute(sql)
+        rs = cursor.fetchall()
+        for row in rs:
+            print(row)
+finally:
+    conn.commit()
+    conn.close()
+
+
 general_bp = Blueprint("general_bp", __name__ , template_folder="templates/general", static_url_path="/static")
 @general_bp.route("/")
 def home():
