3dbb5050af83d910db7136bbc4dce158f618be06
Kim <kimcj@147dda415aea.ant.amazon.com>
2021-04-13 14:21:50 +0900

    edited

--- app/general/general.py
+++ app/general/general.py
@@ -22,7 +22,7 @@ def home():
 
     try:
         with conn.cursor(pymysql.cursors.DictCursor) as cursor:
-            sql = 'select * from fashion;'
+            sql = 'select * from bicycles;'
             cursor.execute(sql)
             rs = cursor.fetchall()
             for row_1 in rs:
