68ccf6963ff20a0bf1b373c4b9eb7eb899473337
Kim <kimcj@147dda415aea.ant.amazon.com>
2021-04-13 17:48:22 +0900

    new load test api2222

--- app/general/general.py
+++ app/general/general.py
@@ -14,8 +14,8 @@ general_bp = Blueprint("general_bp", __name__ , template_folder="templates/gener
 def home():
     return render_template("index.html", title="Home")
 
-@general_bp.route("/api", methods = ['get'])
-def api():
+@general_bp.route("/apiloadtest", methods = ['get'])
+def apiloadtest():
 	return {"id": "octank"}, 200
 
 
