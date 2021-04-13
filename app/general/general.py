63bdf2e9145ca3fb89e05cb4960efe318f6013ad
Kim <kimcj@147dda415aea.ant.amazon.com>
2021-04-13 15:17:25 +0900

    script test

--- app/general/general.py
+++ app/general/general.py
@@ -6,11 +6,7 @@ import json
 import pymysql
 import os
 
-rds_host = os.environ['DATABASE_HOST']
-db_user = os.environ['DATABASE_USER']
-password = os.environ['DATABASE_PASSWORD']
-db_name = os.environ['DATABASE_DB_NAME']
-port = 3306
+
 
 
 
@@ -18,19 +14,14 @@ port = 3306
 general_bp = Blueprint("general_bp", __name__ , template_folder="templates/general", static_url_path="/static")
 @general_bp.route("/")
 def home():
-    conn = pymysql.connect(rds_host, user=db_user, passwd=password, db=db_name, connect_timeout=10000, port=port, charset='utf8mb4')
 
-    try:
-        with conn.cursor(pymysql.cursors.DictCursor) as cursor:
-            sql = 'select * from fashion limit 10;'
-            cursor.execute(sql)
-            rs = cursor.fetchall()
-            for row_1 in rs:
-                for row in rs:
-                    print(row)
-    finally:
-        conn.commit()
-        conn.close()
+    rs = ["load test","load test","load test","load test","load test","load test","load test","load test","load test","load test","load test","load test"]
+    for row_1 in rs:
+        print("getting into second loop")
+        for row2 in rs:
+            print("getting into third loop")
+            for row in rs:
+                print("loop done!")
     return render_template("index.html", title="Home")
 
 @general_bp.route("/analytic")
