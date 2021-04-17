8ced1e73e20790d27b4e5b990532b1b2b6d94c2e
Kim <kimcj@147dda415aea.ant.amazon.com>
2021-04-17 22:25:50 +0900

    db

--- app/models.py
+++ app/models.py
@@ -17,6 +17,7 @@ class Product:
     def __init__(self, product_name=None, db=connect()):
         self.product_name = product_name
         self.cursor = db.cursor(pymysql.cursors.DictCursor)
+        self.db = db
         print("cursor connection done!!!")
         
     
